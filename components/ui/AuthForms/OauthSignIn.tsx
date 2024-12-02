557583dc1471e9b891d6e52c31cbd4dd95319277
Angelo Cortez <angelocortez102@gmail.com>
2024-12-02 14:20:26 -0800

    swithc github for twitch

--- components/ui/AuthForms/OauthSignIn.tsx
+++ components/ui/AuthForms/OauthSignIn.tsx
@@ -3,7 +3,7 @@
 import Button from '@/components/ui/Button';
 import { signInWithOAuth } from '@/utils/auth-helpers/client';
 import { type Provider } from '@supabase/supabase-js';
-import { Github } from 'lucide-react';
+import { Twitch } from 'lucide-react';
 import { useState } from 'react';
 
 type OAuthProviders = {
@@ -15,9 +15,9 @@ type OAuthProviders = {
 export default function OauthSignIn() {
   const oAuthProviders: OAuthProviders[] = [
     {
-      name: 'github',
-      displayName: 'GitHub',
-      icon: <Github className="h-5 w-5" />
+      name: 'twitch',
+      displayName: 'Twitch',
+      icon: <Twitch className="h-5 w-5" />
     }
     /* Add desired OAuth providers here */
   ];
