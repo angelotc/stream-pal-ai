15f733d01de30fd01b6f797559d4501c7cb84f0e
Angelo Cortez <angelocortez102@gmail.com>
2024-12-02 20:22:10 -0800

    add more logging

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -26,6 +26,10 @@ export async function POST(req: Request) {
   const sig = req.headers.get('stripe-signature') as string;
   const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
   let event: Stripe.Event;
+  console.log('Webhook request received');
+  console.log('Signature:', sig ? 'Present' : 'Missing');
+  console.log('Webhook Secret:', webhookSecret ? 'Present' : 'Missing');
+  
 
   try {
     if (!sig || !webhookSecret)
