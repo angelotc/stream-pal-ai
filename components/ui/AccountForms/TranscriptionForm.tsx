a59afe631c9f82226c54f270bf5916bb884c193e
Angelo Cortez <angelocortez102@gmail.com>
2024-12-05 11:48:37 -0800

    remove loggging

--- components/ui/AccountForms/TranscriptionForm.tsx
+++ components/ui/AccountForms/TranscriptionForm.tsx
@@ -54,8 +54,6 @@ export default function TranscriptionForm() {
       }
 
       if (isFinal && speechFinal && thisCaption.trim() !== "") {
-        console.log('isFinal and speechFinal are true');
-        console.log('thisCaption:', thisCaption);
         const now = new Date();
         const timestamp = now.toLocaleTimeString();
         
