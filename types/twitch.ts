d94681d50d9796814f3e9b415e392b9dcee32c00
Angelo Cortez <angelocortez102@gmail.com>
2024-12-07 21:15:28 -0800

    test

--- types/twitch.ts
+++ types/twitch.ts
@@ -1,15 +1,14 @@
 export interface TokenRefresh {
-    twitch_secret: string;
-    twitch_client: string;
-  }
-  
-  export interface TokenValidation {
-    twitchToken: string;
-  }
-  
-  export interface Twitch {
-    client_id: string;
-    access_token: string;
-    twitch_username: string;
-  }
-  
+  twitch_secret: string;
+  twitch_client: string;
+}
+
+export interface TokenValidation {
+  twitchToken: string;
+}
+
+export interface Twitch {
+  client_id: string;
+  access_token: string;
+  twitch_username: string;
+}
