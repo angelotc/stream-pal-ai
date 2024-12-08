60deb75b013324da2fe13d41353ccec71ec59b3b
Angelo Cortez <angelocortez102@gmail.com>
2024-12-07 23:39:59 -0800

    use unstable cache for nwo

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -1,39 +1,59 @@
 import { SupabaseClient } from '@supabase/supabase-js';
-import { cache } from 'react';
+import { unstable_cache } from 'next/cache';
 
-export const getUser = cache(async (supabase: SupabaseClient) => {
-  const {
-    data: { user }
-  } = await supabase.auth.getUser();
-  return user;
-});
+export const getUser = async (supabase: SupabaseClient) => {
+  return unstable_cache(
+    async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      return user;
+    },
+    ['user'],
+    { revalidate: 60 }
+  )();
+};
 
-export const getSubscription = cache(async (supabase: SupabaseClient) => {
-  const { data: subscription, error } = await supabase
-    .from('subscriptions')
-    .select('*, prices(*, products(*))')
-    .in('status', ['trialing', 'active'])
-    .maybeSingle();
+export const getSubscription = async (supabase: SupabaseClient) => {
+  return unstable_cache(
+    async () => {
+      const { data: subscription } = await supabase
+        .from('subscriptions')
+        .select('*, prices(*, products(*))')
+        .in('status', ['trialing', 'active'])
+        .maybeSingle();
+      return subscription;
+    },
+    ['subscription'],
+    { revalidate: 60 }
+  )();
+};
 
-  return subscription;
-});
+export const getProducts = async (supabase: SupabaseClient) => {
+  return unstable_cache(
+    async () => {
+      const { data: products } = await supabase
+        .from('products')
+        .select('*, prices(*)')
+        .eq('active', true)
+        .eq('prices.active', true)
+        .order('metadata->index')
+        .order('unit_amount', { referencedTable: 'prices' });
+      return products;
+    },
+    ['products'],
+    { revalidate: 3600 }
+  )();
+};
 
-export const getProducts = cache(async (supabase: SupabaseClient) => {
-  const { data: products, error } = await supabase
-    .from('products')
-    .select('*, prices(*)')
-    .eq('active', true)
-    .eq('prices.active', true)
-    .order('metadata->index')
-    .order('unit_amount', { referencedTable: 'prices' });
-
-  return products;
-});
-
-export const getUserDetails = cache(async (supabase: SupabaseClient) => {
-  const { data: userDetails } = await supabase
-    .from('users')
-    .select('*')
-    .single();
-  return userDetails;
-});
+export const getUserDetails = async (supabase: SupabaseClient) => {
+  return unstable_cache(
+    async () => {
+      const { data: userDetails } = await supabase
+        .from('users')
+        .select('*')
+        .single();
+      return userDetails;
+    },
+    ['user-details'],
+    { revalidate: 60 }
+  )();
+};
