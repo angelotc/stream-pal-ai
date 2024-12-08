19556d7221849f469be14def3a8e38ae400cc6be
Angelo Cortez <angelocortez102@gmail.com>
2024-12-07 23:29:01 -0800

    downgrade react

--- utils/supabase/queries.ts
+++ utils/supabase/queries.ts
@@ -1,59 +1,39 @@
 import { SupabaseClient } from '@supabase/supabase-js';
-import { unstable_cache } from 'next/cache';
+import { cache } from 'react';
 
-export const getUser = async (supabase: SupabaseClient) => {
-  return unstable_cache(
-    async () => {
-      const { data: { user } } = await supabase.auth.getUser();
-      return user;
-    },
-    ['user'],
-    { revalidate: 60 }
-  )();
-};
+export const getUser = cache(async (supabase: SupabaseClient) => {
+  const {
+    data: { user }
+  } = await supabase.auth.getUser();
+  return user;
+});
 
-export const getSubscription = async (supabase: SupabaseClient) => {
-  return unstable_cache(
-    async () => {
-      const { data: subscription } = await supabase
-        .from('subscriptions')
-        .select('*, prices(*, products(*))')
-        .in('status', ['trialing', 'active'])
-        .maybeSingle();
-      return subscription;
-    },
-    ['subscription'],
-    { revalidate: 60 }
-  )();
-};
+export const getSubscription = cache(async (supabase: SupabaseClient) => {
+  const { data: subscription, error } = await supabase
+    .from('subscriptions')
+    .select('*, prices(*, products(*))')
+    .in('status', ['trialing', 'active'])
+    .maybeSingle();
 
-export const getProducts = async (supabase: SupabaseClient) => {
-  return unstable_cache(
-    async () => {
-      const { data: products } = await supabase
-        .from('products')
-        .select('*, prices(*)')
-        .eq('active', true)
-        .eq('prices.active', true)
-        .order('metadata->index')
-        .order('unit_amount', { referencedTable: 'prices' });
-      return products;
-    },
-    ['products'],
-    { revalidate: 3600 }
-  )();
-};
+  return subscription;
+});
 
-export const getUserDetails = async (supabase: SupabaseClient) => {
-  return unstable_cache(
-    async () => {
-      const { data: userDetails } = await supabase
-        .from('users')
-        .select('*')
-        .single();
-      return userDetails;
-    },
-    ['user-details'],
-    { revalidate: 60 }
-  )();
-};
+export const getProducts = cache(async (supabase: SupabaseClient) => {
+  const { data: products, error } = await supabase
+    .from('products')
+    .select('*, prices(*)')
+    .eq('active', true)
+    .eq('prices.active', true)
+    .order('metadata->index')
+    .order('unit_amount', { referencedTable: 'prices' });
+
+  return products;
+});
+
+export const getUserDetails = cache(async (supabase: SupabaseClient) => {
+  const { data: userDetails } = await supabase
+    .from('users')
+    .select('*')
+    .single();
+  return userDetails;
+});
